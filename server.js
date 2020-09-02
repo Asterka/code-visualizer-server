c7da8e24b71f575737009c92686c2d441649d2b8
asterka <askar.master@yandex.ru>
2020-09-02 17:04:14 -0400

    modified:   package-lock.json 	modified:   package.json 	modified:   server.js

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
 
 const express = require('express')
 const fs = require('fs')
-const path = require('path')
+const path = require('path');
+const e = require('express');
 const app = express()
+const parseString = require('xml2js').parseString;
 
-app.get('/', function(req, res) {
-	res.sendFile(path.join(__dirname + '/res/measures.xml'))
+const measures = fs.readFileSync(`${__dirname}/res/measures.xml`, "utf-8")
+
+
+function findMetricByClass(classMetric, parsedXMLMetrics, obj){
+	obj = { "name":classMetric }
+
+	parsedXMLMetrics["METRICS"]["METRIC"].forEach(element => {
+		if(element["$"].name === classMetric){
+			element["VALUE"].forEach(value => {
+				obj[value["$"]["measured"]] = {"className":value["$"]["measured"], "value": value["$"]["value"]}
+			});
+		}
+	});
+	return obj;
+}
+
+app.get('/api', function(req, res) {
+	//Get the requested metric name
+	const classOfMetric = req.query.metric;
+	var response = "";
+	let obj = {}
+	const result = parseString(measures, function(err, result){
+		obj = findMetricByClass(classOfMetric, result, obj);
+	})
+	res.status(200).json(obj)
 })
+
+
 
 
 let port = 3001;
