52d2537422748709f9c612925d3d63d2de382ef5
asterka <askar.master@yandex.ru>
2021-05-09 23:35:47 +0300

    Removed redundant code, some fixes

--- server.js
+++ server.js
@@ -13,6 +13,26 @@ const originalPath = __dirname;
 app.use(cors());
 app.use(fileUpload());
 
+// Project GET endpoint
+
+app.get("/projects", (req, res) => {
+  let user_token = req.query.token;
+
+  const path = `${originalPath}/res/deps/${user_token}`;
+  let result_folders = [];
+
+  fs.readdir(path, (err, folders) => {
+    if (folders !== undefined) {
+      folders.forEach((project) => {
+        result_folders.push(project);
+      });
+      res.json({ projects: result_folders });
+    } else {
+        res.json({err: "You have no uploaded projects"});
+    }
+  });
+});
+
 // Upload Endpoint
 // TODO obtain token here
 app.post("/upload", (req, res) => {
@@ -21,14 +41,13 @@ app.post("/upload", (req, res) => {
   }
 
   const file = req.files.file;
-  const token = req.body.token.split("-").join("").slice(0, 10);
-  console.log(token);
+  const token = req.body.token;
   const fileName = file.name;
   const ext = fileName.split(".")[fileName.split(".").length - 1];
   let project_token = uuidv4().split("-").join("").slice(0, 7);
 
   exec(
-    `mkdir ${originalPath}/res/upload_dir/${token} && mkdir ${originalPath}/res/upload_dir/${token}/${project_token}`,
+    `mkdir -p ${originalPath}/res/upload_dir/${token}/${project_token}`,
     (error, stdout, stderr) => {
       if (error) {
         console.log(`error: ${error}`);
@@ -39,64 +58,30 @@ app.post("/upload", (req, res) => {
         return;
       }
 
-      file.mv(`${__dirname}/res/upload_dir/${token}/${project_token}/${file.name}`, (err) => {
-        if (err) {
-          console.error(err);
-          return res.status(500).send(err);
-        }
-        let result = new Promise((resolve, reject) => {
-          depsFromJar(fileName, token, project_token, resolve);
-        });
-        result.then((result) => {
-          res.json({
-            fileName: file.name,
-            filePath: `/upload_dir/${token}/${project_token}/${file.name}`,
-            data: result,
-          });
-        });
-      });
-    }
-  );
-  
-});
-
-app.listen(5000, () => console.log("Server Started..."));
-
-function buildProject() {
-  exec(
-    `cd ${originalPath}/res/loadedRepo/jadx-master && gradle wrapper --gradle-version=6.7 && ./gradlew dist`,
-    (error, stdout, stderr) => {
-      if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-      }
-      if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-      }
-      //console.log(`stdout: ${stdout}`);
-      exec(
-        `jdeps --dot-output ${originalPath}/res/deps ${originalPath}/res/loadedRepo/jadx-master/build/jadx/lib/* `,
-        (error, stdout, stderr) => {
-          if (error) {
-            console.log(`error: ${error.message}`);
-            return;
+      file.mv(
+        `${__dirname}/res/upload_dir/${token}/${project_token}/${file.name}`,
+        (err) => {
+          if (err) {
+            console.error(err);
+            return res.status(500).send(err);
           }
-          if (stderr) {
-            console.log(`stderr: ${stderr}`);
-            return;
-          }
-          getDeps(`./res/deps`).catch((err) => {
-            console.log(err);
+          let result = new Promise((resolve, reject) => {
+            depsFromJar(fileName, token, project_token, resolve);
           });
-          setTimeout(() => {
-            console.log(deps);
-          }, 2000);
+          result.then((result) => {
+            res.json({
+              fileName: file.name,
+              filePath: `/upload_dir/${token}/${project_token}/${file.name}`,
+              data: result,
+            });
+          });
         }
       );
     }
   );
-}
+});
+
+app.listen(5000, () => console.log("Server Started..."));
 
 const depsFromJar = (proj_name, token, project_token, resolve) => {
   exec(
@@ -118,7 +103,12 @@ const depsFromJar = (proj_name, token, project_token, resolve) => {
               console.log(`stderr: ${stderr}`);
               return;
             }
-            getDeps(`./res/deps/${token}/${project_token}`, token, project_token, proj_name).catch((err) => {
+            getDeps(
+              `./res/deps/${token}/${project_token}`,
+              token,
+              project_token,
+              proj_name
+            ).catch((err) => {
               console.log(err);
             });
 
@@ -132,83 +122,62 @@ const depsFromJar = (proj_name, token, project_token, resolve) => {
   );
 };
 
-function findFolder(name) {
-  const res = new ff(name.split(".").join("/"), {
-    nottraversal: ["node_modules", "project_files", "res"], //Do not look into unnecessary folders
-  })[0];
-  return res;
-}
-
-//TODO replace the variable part
 
 let deps = {};
 
 async function getDeps(path, token, project_token, proj_name) {
-  console.log(path);
   const dir = await fs.promises.opendir(path);
   for await (const dirent of dir) {
-    fs.readFile(__dirname + `/res/deps/${token}/${project_token}/${dirent.name}`, function (err, data) {
-      if (err) {
-        throw err;
-      }
-      //All deps
-      deps[dirent.name] = {};
-      let dep = data.toString();
-      //TODO REGEXP matching
-      dep = dep.slice(dep.indexOf("{") + 2, dep.indexOf("}")).split("\n");
-
-      dep.forEach((element) => {
-        //only take care of strings that contain "->" symbol (class dependencies definition)
-
-        if (element.indexOf("->") != -1) {
-          splittedTempArray = element.split('"');
-          packageName = splittedTempArray[1];
-          const relPath = __dirname + `/classes/${token}/${project_token}/${proj_name}/`;
-          //get the last significant element, drop the ";" in the end
-          dependsOn = splittedTempArray[splittedTempArray.length - 2];
-          dependsOn = dependsOn.split(" ")[0];
-          //if there is such a PACKAGE array
-          if (Object.keys(deps[dirent.name]).indexOf(packageName) != -1) {
-            //Look for all the class files of the target package to see if it depends on them
-            let file = packageName.split(".");
-            file = file.slice(0, -1);
-            file = relPath + file.join("/") + ".java";
-
-            // //In case a file is not
-            // if (!fs.existsSync(packageAddress)){
-            //     console.log("no dir ",packageAddress);
-            //     return;
-            // }
-
-            // //Find all .java class files in the PACKAGE
-            // let files = fs.readdirSync(packageAddress).filter((file)=>{
-            //     if(file.indexOf('.java') === file.length-5){
-            //         return file;
-            //     }
-            // });
-            // files = files.map((file)=>{return packageAddress+ '/' + file});
-            // // /console.log(files);
-            const inClasses = [];
-            let dependentClasses = [];
-
-            //files.forEach((file)=>{
-            fs.readFile(file, function (err, data) {
-              if (!err) {
-                if (data.indexOf(dependsOn) != -1) {
-                  dependentClasses.push(file);
-                }
-              } else {
-                //console.log(err);
-              }
-            });
-            //})
-
-            deps[dirent.name][packageName].push({ dependsOn: dependsOn });
-          } else {
-            deps[dirent.name][packageName] = [];
-          }
+    fs.readFile(
+      __dirname + `/res/deps/${token}/${project_token}/${dirent.name}`,
+      function (err, data) {
+        if (err) {
+          throw err;
         }
-      });
-    });
+        //All deps
+        deps[dirent.name] = {};
+        let dep = data.toString();
+        //TODO REGEXP matching
+        dep = dep.slice(dep.indexOf("{") + 2, dep.indexOf("}")).split("\n");
+
+        dep.forEach((element) => {
+          //only take care of strings that contain "->" symbol (class dependencies definition)
+
+          if (element.indexOf("->") != -1) {
+            splittedTempArray = element.split('"');
+            packageName = splittedTempArray[1];
+            const relPath =
+              __dirname + `/classes/${token}/${project_token}/${proj_name}/`;
+            //get the last significant element, drop the ";" in the end
+            dependsOn = splittedTempArray[splittedTempArray.length - 2];
+            dependsOn = dependsOn.split(" ")[0];
+            //if there is such a PACKAGE array
+            if (Object.keys(deps[dirent.name]).indexOf(packageName) != -1) {
+              //Look for all the class files of the target package to see if it depends on them
+              let file = packageName.split(".");
+              file = file.slice(0, -1);
+              file = relPath + file.join("/") + ".java";
+
+              const inClasses = [];
+              let dependentClasses = [];
+
+              fs.readFile(file, function (err, data) {
+                if (!err) {
+                  if (data.indexOf(dependsOn) != -1) {
+                    dependentClasses.push(file);
+                  }
+                } else {
+                  //console.log(err);
+                }
+              });
+
+              deps[dirent.name][packageName].push({ dependsOn: dependsOn });
+            } else {
+              deps[dirent.name][packageName] = [];
+            }
+          }
+        });
+      }
+    );
   }
 }
